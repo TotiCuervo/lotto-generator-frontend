3c7aadd3a168ac61024ccc4589c59f7278c6a38f
Toti Cuervo <totifercuervo@gmail.com>
2023-08-27 22:59:40 -0400

    saving

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -3,13 +3,7 @@ import axios from 'axios'
 const createClient = () =>
     axios.create({
         baseURL: process.env.NEXT_PUBLIC_API_URL,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            'Access-Control-Allow-Headers':
-                'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin',
-        },
+        headers: { 'Content-Type': 'application/json' },
         responseType: 'json',
     })
 
